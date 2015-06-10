f537775541bdffc4db334e0d856223254312c497
tzhao95 <tzhao95@gmail.com>
2015-06-10 18:01:38 -0400

    added photoresitor and 2nd LED

--- IFHubLight/IFHubLight.ino
+++ IFHubLight/IFHubLight.ino
@@ -1,38 +1,49 @@
-const int ledPin = 4;
-const int sensor = A0;
-const int switchPin = 3;
+const int RLed = 4;//red led
+const int GLed = 5;//Green led
+const int Tsensor = A1;//temperature sensor
+const int Lsensor = A0;//photoresistor sensor
+const int switchPin = 3;//switch
 const float baseT = 21.0;
+const int baseL = 700;
 
 int count = 10;
 int switchstate = 0;
-int ledState = 0;
+int RState = 0;
+int GState = 0;
+int Lsensorvalue = 0;
+int ledPin = 0;
+float temp = 0;
+float volts = 0;
 //long previousMillis = 0;
 
 //long interval = 1000;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(RLed, OUTPUT);
+  pinMode(GLed, OUTPUT);
   pinMode(switchPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop () {
-  //unsigned long currentMillis = millis()
-  int sensorValue = analogRead(sensor);
-  switchstate = digitalRead(switchPin);
-  //Serial.print("Sensor Value: ");
-  //Serial.print(sensorValue);
-  float volts = (sensorValue/1024.0) * 5.0;
-  //Serial.print(" , Volts: ");
-  //Serial.print(volts);
-  float temp = (volts - 0.5) * 100;
-  Serial.print(", Degrees C: ");
-  Serial.print(temp);
-  Serial.println();
+  readValues();
   if (count >= 10) {
+    if(Lsensorvalue < baseL) {
+      ledPin = GLed;
+    }
+    else {
+      digitalWrite(GLed, 0); 
+    }
     if(temp > baseT) {
+      ledPin = RLed;
+    }
+    else {
+      digitalWrite(RLed, 0); 
+    }
+    if (ledPin != 0) {
       int blinker = 0;
       while(blinker <= 30) {
+        readValues();
         digitalWrite(ledPin, HIGH);
         delay(250);
         digitalWrite(ledPin, LOW);
@@ -41,12 +52,9 @@ void loop () {
         if (switchstate == 0) {
           blinker = 30;
         }
-      blinker++;
+        blinker++;
       }
-      //digitalWrite(ledPin, 1);
-    }
-    else {
-      digitalWrite(ledPin, 0); 
+      ledPin = 0;
     }
     if(switchstate == 0) {
       digitalWrite(ledPin, 0);
@@ -60,17 +68,37 @@ void loop () {
   }
 }
 
-void blink() {
+void blink(int ledPin) {
   int blinker = 0;
-  while(blinker <= 30) {
-    digitalWrite(ledPin, HIGH);
-    delay(250);
-    digitalWrite(ledPin, LOW);
-    delay(250);
-    switchstate = digitalRead(switchPin);
-    if (switchstate = 1) {
-      blinker = 30;
-    }
+    while(blinker <= 30) {
+       digitalWrite(ledPin, HIGH);
+       delay(250);
+       digitalWrite(ledPin, LOW);
+       delay(250);
+       switchstate = digitalRead(switchPin);
+       if (switchstate == 0) {
+         blinker = 30;
+         ledPin = 0;
+       }
     blinker++;
   }
+}
+
+void readValues () {
+  //unsigned long currentMillis = millis()
+  Lsensorvalue = analogRead(Lsensor);
+  int sensorValue = analogRead(Tsensor);
+  switchstate = digitalRead(switchPin);
+  //Serial.print("Sensor Value: ");
+  //Serial.print(sensorValue);
+  volts = (sensorValue/1024.0) * 5.0;
+  //Serial.print(" , Volts: ");
+  //Serial.print(volts);
+  temp = (volts - 0.5) * 100;
+  Serial.print(", Degrees C: ");
+  Serial.print(temp);
+  Serial.println();
+  Serial.print("Raw Sensor Value \t:");
+  Serial.print(Lsensorvalue);
+  Serial.println();
 }
